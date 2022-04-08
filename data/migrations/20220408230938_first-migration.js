f337d6280294ffe11f18e60f95bedfa522acf70e
Daniel Kneale <82185529+Crimson-Beast@users.noreply.github.com>
2022-04-08 16:54:42 -0700

    dbconfig and eslint init

--- data/migrations/20220408230938_first-migration.js
+++ data/migrations/20220408230938_first-migration.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
     return knex.schema.createTable('users', users => {
       users.increments();
       users.string('username', 255).notNullable().unique();
-      users.string('phone',10).notNullable().unique()
+      users.string('phone',10).notNullable().unique();
       users.string('password', 255).notNullable();
     });
   };
