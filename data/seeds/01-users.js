4c93a44107e47dbd155a9aa87c133bde7cef9260
Daniel Kneale <82185529+Crimson-Beast@users.noreply.github.com>
2022-04-08 16:22:14 -0700

    set up knex migration and seed

---
+++ data/seeds/01-users.js
@@ -0,0 +1,11 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {id: 1, username: 'ronSwanson',phone:'123-123-1234',password:'**************'}
+      ]);
+    });
+};
